da3d7af4ce86181a5fbae738093d2cd417be6916
govind2020 <govind@setoo.co>
2023-05-19 12:35:18 +0530

    feat(contactUs) : create mock data and integrate form and ContactUs page need to alignment

--- src/components/form.js
+++ src/components/form.js
@@ -1,46 +1,84 @@
-import React from 'react'
+import { React, useEffect, useState } from "react";
+import { get } from "@/client/api";
 
 const Form = () => {
+  const [contactData, setContactData] = useState([]);
+
+  useEffect(() => {
+    get("contactUs").then((response) => {
+      console.log("data==>", response);
+      setContactData(response);
+    });
+  }, []);
+
   return (
-    <div className='wrapper lg:w-1/2'>
-  
-<form action='https://forms.zohopublic.in/setoosolutionsprivatelimited/form/ContactUsJamstackplus/formperma/Pp2Ws2PM0DANC2AnR60st_Y1vzc5y2QYa3qUYgzzfgY/htmlRecords/submit' name='form' id='form' method='POST' accept-charset='UTF-8' enctype='multipart/form-data'>
-<input type="hidden" name="zf_referrer_name" value=""/>
-<input type="hidden" name="zf_redirect_url" value=""/>
-<input type="hidden" name="zc_gad" value=""/>
+    <div className="wrapper lg:w-1/2">
+      <form
+        action="https://forms.zohopublic.in/setoosolutionsprivatelimited/form/ContactUsJamstackplus/formperma/Pp2Ws2PM0DANC2AnR60st_Y1vzc5y2QYa3qUYgzzfgY/htmlRecords/submit"
+        name="form"
+        id="form"
+        method="POST"
+        accept-charset="UTF-8"
+        enctype="multipart/form-data"
+      >
+        <input type="hidden" name="zf_referrer_name" value="" />
+        <input type="hidden" name="zf_redirect_url" value="" />
+        <input type="hidden" name="zc_gad" value="" />
 
+        <input
+          type="text"
+          name="SingleLine"
+          fieldType={1}
+          maxlength="255"
+          placeholder={contactData?.inputName}
+        />
 
-<label> 
-</label>
-<input  type="text" name="SingleLine"  fieldType={1} maxlength="255" placeholder="Name" />
+        <label></label>
+        <input
+          type="text"
+          maxlength="255"
+          name="Email"
+          fieldType={9}
+          placeholder={contactData?.inutEmal}
+        />
 
-<label> 
-</label>
-<input type="text" maxlength="255" name="Email"  fieldType={9} placeholder="Email" />
+        <label>
+          {contactData?.productName}
+          <em>*</em>
+        </label>
+        <div>
+          <input type="checkbox" id="Checkbox_1" name="Checkbox" value="WebJ" />
+          <label for="Checkbox_1">WebJ</label>
+          <input
+            type="checkbox"
+            id="Checkbox_2"
+            name="Checkbox"
+            value="EcommJ"
+          />
+          <label for="Checkbox_2">EcommJ</label>
+          <input type="checkbox" id="Checkbox_3" name="Checkbox" value="MobJ" />
+          <label for="Checkbox_3">MobJ</label>
+          <input
+            type="checkbox"
+            id="Checkbox_4"
+            name="Checkbox"
+            value="Custom"
+          />
+          <label for="Checkbox_4">Custom</label>
+        </div>
 
-<label>Products
-<em>*</em>
-</label>
-<div>
-
-
-<input type="checkbox" id="Checkbox_1" name="Checkbox" value="WebJ"/>
-<label for="Checkbox_1">WebJ</label>
-<input type="checkbox" id="Checkbox_2" name="Checkbox" value="EcommJ"/>
-<label for="Checkbox_2">EcommJ</label>
-<input type="checkbox" id="Checkbox_3" name="Checkbox" value="MobJ"/>
-<label for="Checkbox_3">MobJ</label>
-<input type="checkbox" id="Checkbox_4" name="Checkbox" value="Custom"/>
-<label for="Checkbox_4">Custom</label>
-
-</div>
-
-<label> 
-</label>
-<textarea name="MultiLine" maxlength="65535" placeholder="Project&#x20;Details" ></textarea>
-<button type="submit"><em>Submit</em></button></form>  
+        <label></label>
+        <textarea
+          name="MultiLine"
+          maxlength="65535"
+          placeholder={contactData?.placeHolder}
+        ></textarea>
+        <button type="submit">
+          <em>{contactData?.buttonLabel}</em>
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,65 +1,189 @@
+import { React, useEffect, useState } from "react";
 import Form from "@/components/form";
-
+import Header from "../components/header";
+import Footer from "../components/footer";
+import { get } from "@/client/api";
 
 const About = () => {
-    return (
+  const [contactData, setContactData] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [hoverItem, setHoverItem] = useState(null);
+
+  useEffect(() => {
+    get("contactUs").then((response) => {
+      console.log("data==>", response);
+      console.log("response==>", response?.caseStudy?.itemList[0]);
+
+      setContactData(response);
+      setSelectedItem(
+        response?.caseStudy?.itemList?.length
+          ? response?.caseStudy?.itemList[0]
+          : {}
+      );
+    });
+  }, []);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const onMouseEnter = (card) => {
+    setHoverItem(card);
+  };
+  const onMouseLeave = () => {
+    setHoverItem(null);
+  };
+
+  return (
     <div className="container">
-         {/* Header Section */}
-      <div className=" h-7 mb-6 ">
-        <h2 className="text-center text-4xl">Header</h2>
-      </div>
+      {/* Header Section */}
+      <Header />
 
       {/* form section */}
-        <div className="lg:pl-24 pt-24 pb-24">
-            <h2 className="lg:w-7/12">Let’s prepare your uniquely engineered digital presence together!</h2>
-            <p className="w-1/2">We are eager to connect with you in the journey of enhancing the PLUS in your business. Help us to know more about you.</p>
+      <div className="lg:pl-24 pt-24 pb-24">
+        <h2 className="lg:w-7/12">
+          {contactData?.heading}
+        </h2>
+        <p className="w-1/2">
+        {contactData?.description}
+        </p>
 
-            <Form/>
-        </div>
+        <Form />
+      </div>
 
-    {/* cta section */}
-    <div className="pl-24">
-        <h3 className="lg:w-8/12 pb-5">If you prefer, send us an email for new business opportunities or job openings.</h3>
-        <a href="mailto: hello@jamstack.plus" className="text-primary heading-4"> hello@jamstack.plus</a>
-
-    </div>
-
+      {/* cta section */}
+      <div className="pl-24">
+        <h3 className="lg:w-8/12 pb-5">
+            {contactData?.contactDetails}
+        </h3>
+        <a
+          href="mailto: hello@jamstack.plus"
+          className="text-primary heading-4"
+        >
+          {contactData?.contactEmail}
+        </a>
+      </div>
 
       {/* case study section */}
-      <div className=" border-t-2 border-b-2 pb-24 border-blue-600">
-        <h2 className="text-4xl text-center ">Case Study</h2>
-      </div>
+      <div className="">
+        {contactData?.caseStudy?.heading && (
+          <h2 className="lg:pl-16">{contactData?.caseStudy?.heading}</h2>
+        )}
+        <div className="flex flex-col lg:flex-row">
+          <div className="lg:w-5/12 p-2 lg:p-10">
+            <ul className="p-1 lg:  flex flex-col gap-6">
+              {contactData?.caseStudy?.itemList.map((item, index) => (
+                <li
+                  className={`flex gap-4 items-center cursor-pointer`}
+                  onClick={() => handleItemClick(item)}
+                  key={index}
+                >
+                  {item === selectedItem && (
+                    <img className="w w-6 h-6" src={item.icon} alt="icon" />
+                  )}
+                  <h4
+                    className={`${
+                      item === selectedItem
+                        ? "font-bold text-black text-5xl"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    {item.label}
+                  </h4>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="lg:w-7/12 lg:p-10 lg:px-7">
+            <div className=" p-5 bg-accent-100">
+              <div className="flex flex-col lg:flex-row  gap-5 lg:gap-0">
+                {selectedItem?.percentageList?.map(
+                  (percentageItem, index) => (
+                    <div className={`cursor-pointer`} key={index}>
+                      <h2>{percentageItem.percentage}</h2>
+                      <p className="w-10/12">{percentageItem.description}</p>
+                    </div>
+                  )
+                )}
+              </div>
+              <div className="pt-5 ">
+                <div>
+                  <a className="flex gap-3 items-center text-primary" href="#">
+                    {contactData?.caseStudy?.label}
+                    <span className="icon-arrow-right2 text-2xl text-primary "></span>
+                  </a>
+                </div>
+              </div>
+            </div>
 
-
-      {/* Testimonial section */}
-      <div className="pb-24 border-t-2 border-b-2 border-red-600">
-        <h2 className="text-4xl text-center ">Testimonial</h2>
-        <div className="flex flex-col lg:flex-row  ">
-          <div className="lg:w-5/12 border-2 px-10 py-2">section1</div>
-          <div className="lg:w-7/12 border-2 px-10 py-2">Section2</div>
+            {contactData?.caseStudy?.description && (
+              <div className="pt-10">
+                <p>{contactData?.caseStudy?.description}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
+      {/* Testimonial section */}
+      <div className="container px-5  gap-24 flex flex-col">
+        {contactData?.testimonialCard?.cards.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col sm:flex-row gap-6 p-3  drop-shadow-2xl justify-evenly items-center  py-5 rounded-3xl border border-black hover:bg-accent-100 hover:border-transparent"
+          >
+            <div className="flex gap-7">
+              <div className="w-4/12 lg:w-3/12 flex items-start sm:justify-center mt-7 sm:mt-0">
+                <img src={item.image} />
+              </div>
+              <div className="flex-grow sm:text-left w-5/12 lg:w-10/12  mt-6 sm:mt-0">
+                <p className="leading-relaxed text-base pb-3">
+                  {item.description}
+                </p>
+                <p>{item.name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
 
-          {/* Blog card section */}
-          <div className="pb-24 border-t-2 border-b-2 border-blue-500">
-        <h2 className="text-center text-4xl">Blog card</h2>
+      {/* Blog card section */}
+      <div className="container px-5 py-10 mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:flex justify-center gap-7">
+          {contactData?.blogCard?.cards.map((card, index) => (
+            <div
+              key={index}
+              className="border-2 border-accent-100 border-opacity-60 lg:max-w-sm rounded-2xl py-1 hover:bg-accent-100"
+              onMouseEnter={() => onMouseEnter(card)}
+              onMouseLeave={() => onMouseLeave()}
+            >
+              <img
+                className="lg:h-48 md:h-36 w-full object-cover object-center rounded-t-2xl"
+                src={card.image}
+                alt={card.altTag}
+              />
+              <div className="p-4">
+                <h4 className="mb-3">{card.label}</h4>
+                <p className="leading-relaxed mb-5 w-9/12">
+                  {card.description}
+                </p>
+                <div>
+                  <a className="flex gap-3 items-center text-primary" href="#">
+                    {contactData?.blogCard?.label}
+                    <span className="icon-arrow-right2 text-2xl text-primary "></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* footer section */}
-      <div className="">
-        <h2 className="text-center text-4xl">Footer</h2>
+      <div>
+        <Footer />
       </div>
-    
-    </div> 
-
-
-
-
-
-    
-          
-        
-    )
-}
+    </div>
+  );
+};
 export default About;
