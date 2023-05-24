63827aa436c3ea5fde59b20644a0518c043233f5
govind2020 <govind@setoo.co>
2023-05-24 12:56:07 +0530

    feat(blog): work in process

---
+++ src/components/blog/blogPage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import LinkButton from '../Link/Link'
+
+export default function Blogpage({ cards, hasIcon, label }) {
+  return (
+    <div className='pt-32'>
+    {cards && cards.map((item, index) => (
+      <div key={index} className="lg:flex items-start pb-32 ">
+        <div className="lg:w-2/5 pr-4">
+          <img  className="w-96 h-auto rounded-3xl " src={item.image} />
+        </div>
+        <div className="w-ful ">
+          <div>
+            <h4>{item.heading}</h4>
+          </div>
+          <div className="pt-4">
+            <div className="flex gap-4">
+              <img className="w-8" src={item.profil} />
+              <p className="w-15">{item.name}</p>
+            </div>
+          </div>
+          <div className="pt-7">
+            <LinkButton label={label} hasIcon={hasIcon} href={item.href} />
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+
+    )
+}
+
---
+++ src/pages/blog.js
@@ -0,0 +1,27 @@
+import { React, useEffect, useState } from "react";
+import { get } from "@/client/api";
+import Layout from '@/components/layout'
+import Blogpage from "@/components/blog/Blogpage";
+
+function Blog() {
+
+    const [blogData, setBlogData] = useState([]);
+
+    useEffect(() => {
+      get("blog").then((response) => {
+        setBlogData(response);
+      });
+    }, []);
+  
+
+  return (
+    <Layout>
+        <div>
+            {<Blogpage {...blogData} />}
+        </div>            
+    </Layout>
+
+    )
+}
+
+export default Blog
