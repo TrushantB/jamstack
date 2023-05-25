371097157d4ba160545e16943d702ac608766f10
TrushantB <trushant.ivision@gmail.com>
2023-05-25 19:20:33 +0530

    Fixed mobile view changes

--- src/components/accordian/accordion.js
+++ src/components/accordian/accordion.js
@@ -11,11 +11,11 @@ const Accordion = ({ accordin }) => {
     }
   };
   return (
-    <div className="px-12">
+    <div className="lg:px-12 px-3">
       {accordin?.map((item, index) => (
         <div className="accordion-item border-t-2 border-accent-200 cursor-pointer" key={index}>
           <div
-            className="accordion-title flex justify-between p-5 items-center"
+            className="accordion-title flex justify-between lg:p-5 items-center"
             onClick={() => toggleAccordion(item)}
           >
             <div>
@@ -63,7 +63,7 @@ const Accordion = ({ accordin }) => {
             </div>
           </div>
           {active === item && (
-            <div className="accordion-content  ml-5 pb-5">{item.description}</div>
+            <div className="accordion-content  lg:ml-5 pb-5">{item.description}</div>
           )}
         </div>
       ))}
