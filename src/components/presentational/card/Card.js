b451d3a34e6938bda3a27a0500184699909c822f
govind2020 <govind@setoo.co>
2023-05-26 15:44:16 +0530

    feat(jamStackPlusPage): add component with mockData

--- src/components/presentational/card/Card.js
+++ src/components/presentational/card/Card.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Card = ({ items }) => {
+  console.log("items" , items)
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:w-1/2 gap-5 card">
-        {items.map((item) => (
+        {items?.map((item) => (
           <div
             key={item.class}
             className="bg-accent-100 rounded-3xl flex flex-col font-bold gap-4 py-3 px-6 pt-5"
