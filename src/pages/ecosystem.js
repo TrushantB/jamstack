d92377a43f1817537f76e8fe761244e8dd36b81c
TrushantB <trushant.ivision@gmail.com>
2023-05-26 16:34:06 +0530

    working on code refactoring

--- src/pages/ecosystem.js
+++ src/pages/ecosystem.js
@@ -2,7 +2,6 @@ import { React, useEffect, useState } from "react";
 import { get } from "@/client/api";
 import { EcoSystemBanner } from "@/components/ecoSystemBanner/ecoSystemBanner";
 import Layout from "@/components/layout";
-import Accordion from "@/components/accordian/accordion";
 import ConnectChoose from "@/components/connectChoose/ConnectChoose";
 import EcosystemAccordion from "@/components/ecosystermAccordian/ecosystemAccordian";
 
@@ -24,20 +23,19 @@ const Ecosysterm = ({ header, footer }) => {
     <Layout header={header} footer={footer}>
 
       <div className="px-5 lg:px-24 py-24  " >
-      {<EcoSystemBanner {...ecoData.ecoBanner} />}
+        {<EcoSystemBanner {...ecoData.ecoBanner} />}
       </div>
 
       <div className="pb-24 px-5 lg:px-24">
         <div className=" pb-6">
           <h2>{ecoData.accordinData?.heading}</h2>
         </div>
-      <div className="">
-        {<EcosystemAccordion  {...ecoData.accordinData} />}
-      </div>
+        <div className="">
+          {<EcosystemAccordion  {...ecoData.accordinData} />}
+        </div>
       </div>
 
       {/* Connect check choose section*/}
-
       <div className="lg:px-24 px-5 pb-24 ">
         <ConnectChoose  {...ecoData.ConnectChoose} />
       </div>
