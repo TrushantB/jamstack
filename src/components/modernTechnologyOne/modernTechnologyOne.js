17167ed764aa3365653f4f0973f36b00d461afa8
govind2020 <govind@setoo.co>
2023-05-26 13:14:58 +0530

    feat(webJ): add components with mockData and add CSS

--- src/components/modernTechnologyOne/modernTechnologyOne.js
+++ src/components/modernTechnologyOne/modernTechnologyOne.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import Button from "../form/button/Button";
+import Link from "next/link";
 
-const ModernTechnologyOne = ({heading}) => {
+const ModernTechnologyOne = ({ heading, type, label, size, href }) => {
   return (
     <div className=" bg-secondary flex flex-col items-center  pt-24 pb-32">
-    <h3 className="text-center text-white w-11/12 ">
-      {heading}
-    </h3>
-  </div>
-  )
-}
+      <h3 className="text-center text-white w-11/12 ">{heading}</h3>
+      {label && (
+        <div className="pt-10">
+          <Link href={href}>
+            <Button label={label} type={type} size={size} />
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default ModernTechnologyOne
+export default ModernTechnologyOne;
