d7a7140ea8aaaca2555cf8a39416edf08f1f62c7
govind2020 <govind@setoo.co>
2023-05-26 11:44:45 +0530

    feat(mobJ) :create mobJ integrate with json data add CSS and add button in component

--- src/components/modernTechnologyOne/modernTechnologyOne.js
+++ src/components/modernTechnologyOne/modernTechnologyOne.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Button from '../form/button/Button'
 
-const ModernTechnologyOne = ({heading}) => {
+const ModernTechnologyOne = ({heading , label , size , type}) => {
   return (
     <div className=" bg-secondary flex flex-col items-center  pt-24 pb-32">
     <h3 className="text-center text-white w-11/12 ">
       {heading}
     </h3>
+    { label && <div className='pt-16'>
+      <Button 
+        label={label}
+        type={type}
+        size={size}
+      />
+    </div>}
   </div>
   )
 }
--- src/pages/mobj.js
+++ src/pages/mobj.js
@@ -1,4 +1,4 @@
-import {React , useState , useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import { get } from "@/client/api";
 import Layout from "@/components/layout";
 import Stepper from "@/components/stepper/stepper";
@@ -11,79 +11,64 @@ import ModernTechnologyOne from "@/components/modernTechnologyOne/modernTechnolo
 import Accordion2 from "@/components/platformAccordian/platformAccordian";
 import Cta from "@/components/cta/cta";
 
+const Ecommj = ({ header, footer }) => {
+  const [mobjData, setMobjData] = useState(null);
 
+  useEffect(() => {
+    get("mobj").then((response) => {
+      setMobjData(response);
+    });
+  }, []);
 
-  const Ecommj = ({ header, footer }) => {
-    const [mobjData, setMobjData] = useState(null);
-  
-    useEffect(() => {
-      get("mobj").then((response) => {
-        setMobjData(response);
-        
-      });
-    }, []);
+  console.log("here", mobjData?.cta);
 
-  console.log("here",mobjData?.cta)
-
-    if (!mobjData) {
-      return <></>;
-    }
+  if (!mobjData) {
+    return <></>;
+  }
   return (
     <Layout header={header} footer={footer}>
       {/* Banner section */}
       <div className=" pb-32 pt-32 px-5 lg:px-24 ">
-      <Banner {...mobjData.banner}/>
+        <Banner {...mobjData.banner} />
       </div>
 
       {/* Ecomm section */}
-     <Info {...mobjData.info}/>
-     {console.log(mobjData)}
+      <Info {...mobjData.info} />
 
       {/* Ecomm process */}
       <div className="pb-24 lg:px-24 px-5 ">
-
-        
-          <Stepper {...mobjData?.stepperData}/>
-    
+        <Stepper {...mobjData?.stepperData} />
       </div>
 
       {/* Facts Section with bg color */}
       <div className="">
-        <ModernTechnologyOne {...mobjData?.morderTechnologyOne}/>
-        
+        <ModernTechnologyOne {...mobjData?.morderTechnologyOne} />
       </div>
 
-     
-
       {/* Facts Section with bg color */}
-     
-        <div className="lg:px-24 py-32">
-        {<Accordion2 {...mobjData?.accordian}/>}
-        </div>
-      
-         
-        
-      
+
+      <div className="lg:px-24 py-32">
+        {<Accordion2 {...mobjData?.accordian} />}
+      </div>
 
       {/* case study section */}
       <div className=" pb-24 lg:px-24 px-5">
-       <CaseStudy {...mobjData?.caseStudy}/>
+        <CaseStudy {...mobjData?.caseStudy} />
       </div>
 
       {/* cta section */}
       <div className="flex flex-col bg-accent-100 justify-center items-center">
-       <Cta {...mobjData?.cta}/>
-       
+        <Cta {...mobjData?.cta} />
       </div>
 
       {/* Testimonial section */}
       <div className="py-24 lg:px-24 px-5  ">
-        <TestimonialCard {...mobjData?.testimonialCard}/>
+        <TestimonialCard {...mobjData?.testimonialCard} />
       </div>
 
       {/*  blog section */}
       <div className=" pb-24 px-5 lg:px-24 ">
-       <BlogCard {...mobjData?.blogCard}/>
+        <BlogCard {...mobjData?.blogCard} />
       </div>
     </Layout>
   );
