9645feb19e0e2885af5aa46e7fa99d49564bc29f
TrushantB <trushant.ivision@gmail.com>
2023-05-26 16:32:07 +0530

    working on ecosystem page

--- src/components/ecosystermAccordian/ecosystemAccordian.js
+++ src/components/ecosystermAccordian/ecosystemAccordian.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 
 const EcosystemAccordion = ({ accordin }) => {
   const [active, setActive] = useState({});
@@ -10,6 +10,9 @@ const EcosystemAccordion = ({ accordin }) => {
       setActive({})
     }
   };
+  useEffect(() => {
+    accordin?.length && setActive(accordin[0])
+  }, [accordin])
   return (
     <div className="">
       {accordin?.map((item, index) => (
@@ -19,25 +22,25 @@ const EcosystemAccordion = ({ accordin }) => {
             onClick={() => toggleAccordion(item)}
           >
             <div className="flex items-center gap-4">
-                <div className="diamond w-5 h-5 bg-tertiary"></div>
+              <div className="diamond w-5 h-5 bg-tertiary"></div>
               <h3>{item.label}</h3>
             </div>
-            
+
           </div>
           {active === item && (
-           
+
             <div className="accordion-content flex-col lg:flex-row justify-between flex gap-5 items-center lg:pl-10 pb-5">
-                <div className="lg:w-1/2">
+              <div className="lg:w-1/2">
                 <div className="w-full" dangerouslySetInnerHTML={{ __html: item.content }}></div>
 
-                </div>
-                <div className="w-1/2 flex justify-end items-center">
+              </div>
+              <div className="w-1/2 flex justify-end items-center">
 
-                <img className="w-full h-full" src={`${item.image}`}/>
-                </div>
+                <img className="w-full h-full" src={`${item.image}`} />
+              </div>
             </div>
-         
-            
+
+
           )}
         </div>
       ))}
