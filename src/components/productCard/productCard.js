590a99bc39b3fdce2b0e93c261aceecd3f531702
Murtazakp <khopoliwala52@gmail.com>
2023-05-30 13:11:30 +0530

    feat(card):added animation to product card

--- src/components/productCard/productCard.js
+++ src/components/productCard/productCard.js
@@ -1,41 +1,59 @@
-import { React, useState } from "react";
 import Link from "next/link";
+import gsap from "gsap";
 
 function ProductCard({ cards }) {
-  const [hoverItem, setHoverItem] = useState(null);
-  const onMouseEnter = (card) => {
-    setHoverItem(card);
+  gsap.set(".ancor", { y: -20 });
+  const onMouseEnter = (event) => {
+    const element = event.currentTarget;
+    gsap.to(element, { backgroundColor: "#4767F6", duration: 0.3 });
+    gsap.to(element.querySelector("p"), { color: "#ffffff", duration: 0.3 });
+    gsap.to(element.querySelector("h3"), { color: "#ffffff", duration: 0.3 });
+    gsap.to(element.querySelector(".circle"), {
+      scale: 1.5,
+      transformOrigin: "100% 100%",
+      backgroundColor: "#e002a2"
+    });
+    gsap.to(element.querySelector("a"), {
+      opacity: 1,
+      duration: 0.2,
+      x: 25,
+      color: "#ffffff"
+    });
   };
-  const onMouseLeave = () => {
-    setHoverItem(null);
+
+  const onMouseLeave = (event) => {
+    const element = event.currentTarget;
+    gsap.to(element, { backgroundColor: "" });
+    gsap.to(element.querySelector("p"), { color: "" });
+    gsap.to(element.querySelector("h3"), { color: "" });
+    gsap.to(element.querySelector(".circle"), {
+      scale: 1,
+      transformOrigin: "100% 100%",
+      backgroundColor: "#ff5223"
+    });
+    gsap.to(element.querySelector("a"), { opacity: 0, x: -50 ,duration:0.3});
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:flex justify-center  gap-5 container lg:mx-auto card">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:flex justify-center relative z-40 gap-5 container lg:mx-auto card">
       {cards &&
-        cards?.map((card, index) => (
+        cards.map((card, index) => (
           <div
             key={index}
-            className=" p-6 mx-4 sm:mx-0   rounded-2xl bg-accent-100 flex flex-col justify-between  lg:w-[400px]  hover:bg-secondary hover:text-white"
-            onMouseEnter={() => onMouseEnter(card)}
-            onMouseLeave={() => onMouseLeave()}
+            className="p-6 mx-4 sm:mx-0 rounded-2xl bg-accent-100 flex flex-col justify-between lg:w-[400px] "
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
           >
             <div>
-              <h3 className="font-bold tracking-tight  mb-6 ">{card.label}</h3>
-              <p className="font-normal ">{card.description}</p>
+              <h3 className="font-bold tracking-tight mb-6">{card.label}</h3>
+              <p className="font-normal mb-20 ">{card.description}</p>
             </div>
-            {hoverItem === card ? (
-              <div className="flex justify-end items-center  mt-14">
-                <Link className=" relative z-30 -m-10" href={card.href}>
-                  {card.buttonLabel}
-                </Link>
-                <div className="w-28 h-28 rounded-full bg-primary"></div>
-              </div>
-            ) : (
-              <div className="flex justify-end items-end mt-32">
-                <div className="w-14 h-14 rounded-full bg-tertiary "></div>
-              </div>
-            )}
+            <div className="flex justify-end items-center">
+              <Link href={card.href} className="text-black ancor relative z-20 opacity-0">
+                {card.buttonLabel}
+              </Link>
+              <div className="w-16 h-16 rounded-full bg-[#ff5223] circle"></div>
+            </div>
           </div>
         ))}
     </div>
