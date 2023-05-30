4ad08b081f3e01abcdf4235c9747f6b7d32d2872
Murtazakp <khopoliwala52@gmail.com>
2023-05-30 17:14:01 +0530

    feat(line-animation):added line animation to all home and ecomj webj and mobj

--- src/components/webSection/webSection.js
+++ src/components/webSection/webSection.js
@@ -1,23 +1,48 @@
-import React from "react";
-import Link from "next/link";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
-useEffect(() => {
-    
-}, [])
+function WebSection({ heading, description, lineImage, logo }) {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
 
-function WebSection({ heading, description, lineIMage, logo }) {
+    const tl = gsap.timeline({
+      defaults: { duration: 6, ease: "linear" },
+    });
+
+    tl.fromTo(
+      ".line",
+      { opacity: 0, height: 0 },
+      { opacity: 1, height: "100%", backgroundColor: "black", duration: 6 }
+    )
+      .to(".dot", { duration: 0.5, opacity: 1, scale: 1.2 })
+      .fromTo(
+        ".websection h2,.websection img",
+        { opacity: 0, y: 200 },
+        { opacity: 1, y: 0 }
+      );
+
+    ScrollTrigger.create({
+      trigger: ".websection",
+      start: "20% 30%",
+      end: "bottom center",
+      scrub: true,
+      animation: tl,
+    });
+  }, []);
+
   return (
-    <div className="container  mx-auto lg:px-0 text-black">
-      <div className="flex flex-col items-center lg:pb-24 pb-16  mx-4 lg:mx-0">
-        <p className="mx-auto font-normal text-center heading-4 mb-11 lg:w-10/12 ">
+    <div className="container mx-auto lg:px-0 text-black websection">
+      <div className="flex flex-col items-center lg:pb-24 pb-16 mx-4 lg:mx-0">
+        <p className="mx-auto font-normal text-center heading-4 mb-11 lg:w-10/12">
           {description}
         </p>
-        <div class=" flex justify-between items-center flex-col h-80">
-          <div class="line border-3 w-1 border-green  bg-black"></div>
-          <div class="w-5 h-5 rounded-full bg-black dot opacity-0  "></div>
+        <div className="flex justify-between items-center flex-col h-80">
+          <div className="line border-3 w-[1px] border-green bg-black"></div>
+          <div className="w-5 h-5 rounded-full bg-black dot opacity-0 "></div>
         </div>
-        <h2 className="font-bold text-center lg:w-7/12">{heading}</h2>
-        <img className="mt-5 lg:w-2/12" src={logo?.url} />
+        <h2 className="font-bold text-center lg:w-7/12  ">{heading}</h2>
+        <img className="mt-5 lg:w-2/12" src={logo?.url} alt="Logo" />
       </div>
     </div>
   );
