9ea96c8036b69685961a0ec2c8b6a9314d635f75
Murtazakp <khopoliwala52@gmail.com>
2023-05-30 13:37:23 +0530

    feat(line-animation):wip

--- src/components/webSection/webSection.js
+++ src/components/webSection/webSection.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Link from "next/link";
 
+useEffect(() => {
+    
+}, [])
+
 function WebSection({ heading, description, lineIMage, logo }) {
   return (
     <div className="container  mx-auto lg:px-0 text-black">
@@ -8,9 +12,9 @@ function WebSection({ heading, description, lineIMage, logo }) {
         <p className="mx-auto font-normal text-center heading-4 mb-11 lg:w-10/12 ">
           {description}
         </p>
-        <div className="flex justify-center items-center flex-col pb-5 mx-4 lg:mx-0 ">
-          <div className="w-[1px] h-30 py-32 bg-black"></div>
-          <div className="circle w-5 h-5 bg-black"></div>
+        <div class=" flex justify-between items-center flex-col h-80">
+          <div class="line border-3 w-1 border-green  bg-black"></div>
+          <div class="w-5 h-5 rounded-full bg-black dot opacity-0  "></div>
         </div>
         <h2 className="font-bold text-center lg:w-7/12">{heading}</h2>
         <img className="mt-5 lg:w-2/12" src={logo?.url} />
