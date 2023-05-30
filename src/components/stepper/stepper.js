33f52f2f718098cc94a45f7d9ffe704949a4aaf5
govind2020 <govind@setoo.co>
2023-05-30 18:35:22 +0530

    feat(stepper): changes in stepper id

--- src/components/stepper/stepper.js
+++ src/components/stepper/stepper.js
@@ -1,32 +1,47 @@
-import { React, useState } from "react";
+import { React, useState , useRef } from "react";
 import Button from "../form/button/Button";
 import Accordion from "../accordian/accordion";
 
-
-
 const Stepper = (stepper) => {
+
+
   const [selectedStep, setSelectedStep] = useState(1);
 
   const handleStepClick = (index) => {
     setSelectedStep(index);
   };
 
+
+  scrollWidthOffset = (el) => {
+    const menu = document.getElementById('menu')
+    window.scrollTo({
+        top: el.offsetTop - menu.offsetHeight,
+        behavior: "smooth"
+      });
+  }
+
   return (
     <>
       {/* stepper code */}
-      <div className="pb-28 text-center relative"><h2>{stepper?.heading}</h2></div>
+      <div className="pb-28 text-center relative">
+        <h2>{stepper?.heading}</h2>
+      </div>
       <div className="">
-        <div className="stepper flex justify-center gap-3 lg:gap-5 text-xs  lg:text-sm mb-7 text-center lg:text-left sticky top-0 py-3 bg-white">
+        <div id="menu" className="stepper flex justify-center gap-3 lg:gap-5 text-xs  lg:text-sm mb-7 text-center lg:text-left sticky top-0 py-3 bg-white">
           {stepper?.stepper.map((step, index) => (
-            <a href={`#${step.id}`} key={index}>
+            <a  href={`#${step.id}`} key={index}>
               <div
-                className={`step1 flex flex-col lg:flex-row gap-2 items-center ${selectedStep === index + 1 ? "text-primary" : ""
-                  }`}
+                className={`step1 flex flex-col lg:flex-row gap-2 items-center ${
+                  selectedStep === index + 1 ? "text-primary" : ""
+                }`}
                 onClick={() => handleStepClick(index + 1)}
               >
                 <div
-                  className={`${selectedStep === index + 1 ? "bg-black text-white" : "bg-accent-100 "
-                    } p-3  rounded-full h-5 w-5 flex justify-center items-center`}
+                  className={`${
+                    selectedStep === index + 1
+                      ? "bg-black text-white"
+                      : "bg-accent-100 "
+                  } p-3  rounded-full h-5 w-5 flex justify-center items-center`}
                 >
                   {index + 1}
                 </div>
@@ -39,10 +54,11 @@ const Stepper = (stepper) => {
         {/* stepper section */}
         {stepper?.stepper.map((step, index) => (
           <div
-            className={`flex flex-col gap-5 py-5 lg:gap-0 p-2 lg:p-5 pb-14 lg:my-0 ${step.layout === "imageLeft"
-              ? "lg:flex-row-reverse"
-              : "lg:flex-row"
-              }`}
+            className={`flex flex-col gap-5 py-5 lg:gap-0 p-2 lg:p-5 pb-14 lg:my-0 ${
+              step.layout === "imageLeft"
+                ? "lg:flex-row-reverse"
+                : "lg:flex-row"
+            }`}
             id={step.id}
             key={step.id}
           >
@@ -52,8 +68,13 @@ const Stepper = (stepper) => {
                   <h3 className="pb-5">
                     <span>{index + 1}.</span> {step.label}
                   </h3>
-                  <div className="" dangerouslySetInnerHTML={{ __html: step.content }}></div>
-                  {step?.faqs?.length && <Accordion accordin={step?.faqs} isInner={true} />}
+                  <div
+                    className=""
+                    dangerouslySetInnerHTML={{ __html: step.content }}
+                  ></div>
+                  {step?.faqs?.length && (
+                    <Accordion accordin={step?.faqs} isInner={true} />
+                  )}
                   {step?.cta?.label && <Button {...step?.cta} />}
                 </div>
                 <div className="lg:w-1/2 lg:p-2 flex justify-center items-start lg:mt-10">
