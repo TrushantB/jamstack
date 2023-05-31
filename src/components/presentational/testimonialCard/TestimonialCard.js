8f9811b8540853b7c16f9a86cc9f2cb913c0a157
Murtazakp <khopoliwala52@gmail.com>
2023-05-31 15:28:02 +0530

    feat(testimonial):wip

--- src/components/presentational/testimonialCard/TestimonialCard.js
+++ src/components/presentational/testimonialCard/TestimonialCard.js
@@ -62,7 +62,7 @@ function TestimonialCard({ cards, heading }) {
         <h2 className="mb-5 lg:mb-0">{heading}</h2>
       </div>
 
-      <div className="  gap-14 flex flex-col lg:w-8/12  mx-4 lg:mx-0 testimonial ">
+      <div className="  gap-14 flex flex-col lg:w-8/12  mx-4 lg:mx-0 testimonial lg:hidden ">
         <Slider {...sliderSetting}>
           {cards &&
             cards?.map((item, index) => (
@@ -84,6 +84,30 @@ function TestimonialCard({ cards, heading }) {
               </div>
             ))}
         </Slider>
+      </div>
+
+      <div className="  gap-14 lg:w-8/12  mx-4 lg:mx-0 testimonial hidden lg:block ">
+    
+          {cards &&
+            cards?.map((item, index) => (
+              <div
+                key={index}
+                className="flex flex-col sm:flex-row gap-6 p-3  mb-14  drop-shadow-2xl hover:drop-shadow-none justify-evenly items-center  py-5 rounded-3xl border border-black hover:rounded-3xl hover:bg-accent-100"
+              >
+                <div className="flex gap-7 ">
+                  <div className="w-4/12 lg:w-3/12 flex items-start sm:justify-center mt-7 sm:mt-0">
+                    <img src={item.image} />
+                  </div>
+                  <div className="flex-grow sm:text-left w-5/12 lg:w-10/12  mt-6 sm:mt-0">
+                    <p className="leading-relaxed text-base pb-3">
+                      {item.description}
+                    </p>
+                    <p>{item.name}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+        
       </div>
     </div>
     
