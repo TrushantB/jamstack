5550d15069a90be37671a6133aee4b029aa2e35e
MurtazaKp <99162517+MurtazaKp@users.noreply.github.com>
2023-06-14 11:54:14 +0530

    feat(aboutus):did some changes

--- src/components/svgAnimations/banner/about.js
+++ src/components/svgAnimations/banner/about.js
@@ -1,45 +1,60 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
-import { EcomJBanner } from "@/components/animationSvg/ecomj";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { AboutBanner } from "@/components/animationSvg/about";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const About = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-
-    const ecomj = gsap.timeline({});
+    const about = gsap.timeline();
 
     gsap.set("#Group-1091", { opacity: 0, y: 50 });
-    gsap.set("#long-cylinder", { y:-30, opacity: 0 });
+    gsap.set("#long-cylinder", { y: -30, opacity: 0 });
     gsap.set("#pink-circle", { x: -30, opacity: 0 });
     gsap.set("#Group-1082", { x: -50, opacity: 0 });
-    gsap.set("#Group-1083", {  opacity: 0 });
-    gsap.set("#Group-1087", {  opacity: 0, rotate:180 });
+    gsap.set("#Group-1083", { opacity: 0 });
+    gsap.set("#one", { opacity: 0 });
+    gsap.set("#two", { x: 35, y: 35, opacity: 0, scale: 0, rotate: 180 });
+    gsap.set("#about-circle", { y: -20, opacity: 0 });
+    gsap.set("#Group-1086", { y: -30, opacity: 0 });
+    gsap.set("#white-pentagon", { x: -50, opacity: 0 });
+    gsap.set("#Group-1084", { x: 50, opacity: 0 });
+    gsap.set("#Group-1088", { y: 30, opacity: 0 });
 
-   
+    about
+      .to("#Group-1080", { opacity: 1, duration: 0.25 })
+      .to("#Group-1091", { opacity: 1, y: 0, duration: 0.25 })
+      .to("#long-cylinder", { y: 0, opacity: 1, duration: 0.25 })
+      .to("#pink-circle", { x: 0, opacity: 1, duration: 0.25 })
+      .to("#Group-1082", { x: 0, opacity: 1, duration: 0.25 })
+      .to("#Group-1083", { opacity: 1, duration: 0.25 })
+      .to("#one", { opacity: 1, duration: 0.25 })
+      .to("#two", {
+        opacity: 1,
+        rotate: 0,
+        scale: 1,
+        transformOrigin: "center center",
+        duration: 0.25,
+      })
+      .to("#Group-1086", { y: -0, opacity: 1, duration: 0.25 })
+      .to("#about-circle", { y: 0, opacity: 1, duration: 0.25 })
+      .to("#white-pentagon", { x: 0, opacity: 1, duration: 0.25 })
+      .to("#Group-1084", { x: 0, opacity: 1, duration: 0.25 })
+      .to("#Group-1088", { y: 0, opacity: 1, duration: 0.25 });
 
-    ecomj
-    .to("#Group-1091", { opacity: 1, y: 0 })
-    .to ("#long-cylinder", { y:0, opacity: 1 })
-    .to ("#pink-circle", { x: 0, opacity: 1 })
-    .to ("#Group-1082", { x: 0, opacity: 1 })
-    .to ("#Group-1083", {   opacity: 1,  })
-    .to("#Group-1087", {  opacity: 1, rotate:0,transformOrigin:"center center" });
-
-
-    // ScrollTrigger.create({
-    //   trigger: "#ecomj",
-    //   start: "1% 20%",
-    //   end: "80% 70%",
-    //   animation: ecomj,
-    //   toggleActions: "restart none none reverse",
-    // });
+    ScrollTrigger.create({
+      trigger: ".about",
+      start: "8% 20%",
+      end: "80% 70%",
+      animation: about,
+      toggleActions: "restart none none reverse",
+      markers: true,
+    });
   }, []);
 
   return (
-    <div className="mx-auto lg:w-10/12">
-      <AboutBanner/> 
+    <div id="#about" className="mx-auto lg:w-10/12 about">
+      <AboutBanner />
     </div>
   );
 };
