f7bbb19fcc9f0672fb474507a58a1667f5885949
MurtazaKp <99162517+MurtazaKp@users.noreply.github.com>
2023-06-14 16:03:16 +0530

    refactor(about):changed the id of the elements

--- src/components/svgAnimations/banner/about.js
+++ src/components/svgAnimations/banner/about.js
@@ -8,26 +8,26 @@ const About = () => {
     gsap.registerPlugin(ScrollTrigger);
     const about = gsap.timeline();
 
-    gsap.set("#Group-1091", { opacity: 0, y: 50 });
-    gsap.set("#long-cylinder", { y: -30, opacity: 0 });
-    gsap.set("#pink-circle", { x: -30, opacity: 0 });
-    gsap.set("#Group-1082", { x: -50, opacity: 0 });
-    gsap.set("#Group-1083", { opacity: 0 });
+    gsap.set("#grayUnion", { opacity: 0, y: 50 });
+    gsap.set("#longCylinder", { y: -30, opacity: 0 });
+    gsap.set("#pinkCircle", { x: -30, opacity: 0 });
+    gsap.set("#nestedCircle", { x: -50, opacity: 0 });
+    gsap.set("#nestedPentagon", { opacity: 0 });
     gsap.set("#one", { opacity: 0 });
     gsap.set("#two", { x: 35, y: 35, opacity: 0, scale: 0, rotate: 180 });
-    gsap.set("#about-circle", { y: -20, opacity: 0 });
-    gsap.set("#Group-1086", { y: -30, opacity: 0 });
+    gsap.set("#dotCircle", { y: -20, opacity: 0 });
+    gsap.set("#blueUnion", { y: -30, opacity: 0 });
     gsap.set("#white-pentagon", { x: -50, opacity: 0 });
-    gsap.set("#Group-1084", { x: 50, opacity: 0 });
-    gsap.set("#Group-1088", { y: 30, opacity: 0 });
+    gsap.set("#orangeUnion", { x: 50, opacity: 0 });
+    gsap.set("#nestedDiamond", { y: 30, opacity: 0 });
 
     about
-      .to("#Group-1080", { opacity: 1, duration: 0.25 })
-      .to("#Group-1091", { opacity: 1, y: 0, duration: 0.25 })
-      .to("#long-cylinder", { y: 0, opacity: 1, duration: 0.25 })
-      .to("#pink-circle", { x: 0, opacity: 1, duration: 0.25 })
-      .to("#Group-1082", { x: 0, opacity: 1, duration: 0.25 })
-      .to("#Group-1083", { opacity: 1, duration: 0.25 })
+
+      .to("#grayUnion", { opacity: 1, y: 0, duration: 0.25 })
+      .to("#longCylinder", { y: 0, opacity: 1, duration: 0.25 })
+      .to("#pinkCircle", { x: 0, opacity: 1, duration: 0.25 })
+      .to("#nestedCircle", { x: 0, opacity: 1, duration: 0.25 })
+      .to("#nestedPentagon", { opacity: 1, duration: 0.25 })
       .to("#one", { opacity: 1, duration: 0.25 })
       .to("#two", {
         opacity: 1,
@@ -36,11 +36,11 @@ const About = () => {
         transformOrigin: "center center",
         duration: 0.25,
       })
-      .to("#Group-1086", { y: -0, opacity: 1, duration: 0.25 })
-      .to("#about-circle", { y: 0, opacity: 1, duration: 0.25 })
+      .to("#blueUnion", { y: -0, opacity: 1, duration: 0.25 })
+      .to("#dotCircle", { y: 0, opacity: 1, duration: 0.25 })
       .to("#white-pentagon", { x: 0, opacity: 1, duration: 0.25 })
-      .to("#Group-1084", { x: 0, opacity: 1, duration: 0.25 })
-      .to("#Group-1088", { y: 0, opacity: 1, duration: 0.25 });
+      .to("#orangeUnion", { x: 0, opacity: 1, duration: 0.25 })
+      .to("#nestedDiamond", { y: 0, opacity: 1, duration: 0.25 });
 
     ScrollTrigger.create({
       trigger: ".about",
