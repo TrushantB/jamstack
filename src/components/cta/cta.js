82b61d802a95e7cf97b9bd6089698a9898ef718e
Girish Boke <girish.boke@gmail.com>
2023-06-21 11:30:35 +0530

    Footer & CTA alignment changes

--- src/components/cta/cta.js
+++ src/components/cta/cta.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const Cta = ({heading,label , href,isInner=false}) => {
   return (
     <div className='container mx-auto px-6'>
-         <div className={`flex flex-col bg-accent-100 justify-center py-12 lg:py-24 ${isInner?'items-start ':' items-center '}`}>
+         <div className={`flex flex-col bg-accent-100 justify-center items-center lg:items-start py-12 lg:py-24 ${isInner?'items-start ':' items-center '}`}>
         <h2 className="pb-5 lg:pb-7 text-center lg:text-left">{heading}</h2>
         <Link className='btn' href={href}>
           {label}
--- src/components/presentational/footer/Footer.js
+++ src/components/presentational/footer/Footer.js
@@ -14,8 +14,8 @@ const Footer = ({
     <>
       <div className="container lg:mx-auto mt-7 mb-7 md:mb-0">
         <div className="mx-4 lg:mx-0">
-          <div className="grid grid-cols-12   ">
-            <div className="col-span-12 mt-10 sm:col-span-6 lg:col-span-6 sm:p-5 lg:p-10 lg:pl-0 sm:m-0 lg:pt-0">
+          <div className="grid grid-cols-12">
+            <div className="col-span-12 my-4 lg:my-0 sm:col-span-6 lg:col-span-6 sm:p-5 lg:p-10 lg:pl-0 sm:m-0 lg:pt-0">
               <div>
                 <Link href={`/${footerLogo?.href}`}>
                   <img className="h-10 w-60" src={footerLogo?.url} alt="logo" />
@@ -36,8 +36,8 @@ const Footer = ({
                 </div>
               </div>
             </div>
-            <div className="col-span-12 sm:col-span-6 mt-5 sm:mt-0 ">
-              <ul className="grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 sm:p-0 lg:p-0">
+            <div className="col-span-12 sm:col-span-6 my-5 sm:mt-0 ">
+              <ul className="grid  grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-0 sm:p-0 lg:p-0">
                 {footerMenu?.map((menuItems, index) => {
                   return (
                     <li key={index} className="font-semibold mb-2">
@@ -50,17 +50,17 @@ const Footer = ({
               </ul>
             </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-3 sm:py-5 lg:py-10 lg:mt-5 sm:mt-0 ">
+          <div className="grid grid-cols-1 sm:grid-cols-3 sm:py-5 lg:py-6 lg:mt-0 sm:mt-0">
             <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-6 col-span-2  lg:mx-0">
               {secondaryMenu?.map((menuItems, index) => (
-                <li className="mb-2" key={index}>
+                <li className="mb-2 md:mb-0" key={index}>
                   <Link href={resolveHref(menuItems.href)} className="font-bold btn-link">
                     {menuItems.label}
                   </Link>
                 </li>
               ))}
             </ul>
-            <div className="flex items-center mt-5 sm:items-start sm:justify-end bold sm:mt-0">
+            <div className="flex items-center mt-5 sm:items-start sm:justify-end bold sm:mt-0 text-xs lg:self-center">
               <CustomPortableText
                 paragraphClasses="font-bold"
                 value={copyRight}
